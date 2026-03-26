4b5a701716228790ba50d5e5a7e4349353427fb9
Deva Sutar <sdeva2544@gmail.com>
2026-03-26 16:50:43 +0530

    Added SQL file

---
+++ std.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE std;
+
+USE std;
+
+CREATE TABLE students (
+    id INT,
+    name VARCHAR(50),
+    marks INT
+);
+
+INSERT INTO students VALUES (1,'Rahul',85);
+INSERT INTO students VALUES (2,'Deva',90);
